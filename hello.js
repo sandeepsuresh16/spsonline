ef27a4684a85e0ee277dd1b532e20173ab74eb7e
sandeep <sandeepsuresh16@gmail.com>
2021-09-24 21:59:31 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,20 @@
+function myFunction(){
+    document.getElementById('image1').style.display="none";
+    document.getElementById('vid1').style.display="block";
+}
+function myFunctionTwo(){
+    document.getElementById('image2').style.display="none";
+    document.getElementById('vid2').style.display="block";
+}
+function myFunctionThree(){
+    document.getElementById('image3').style.display="none";
+    document.getElementById('vid3').style.display="block";
+}
+function myFunctionFour(){
+    document.getElementById('image4').style.display="none";
+    document.getElementById('vid4').style.display="block";
+}
+function myFunctionFive(){
+    document.getElementById('image5').style.display="none";
+    document.getElementById('vid5').style.display="block";
+}
